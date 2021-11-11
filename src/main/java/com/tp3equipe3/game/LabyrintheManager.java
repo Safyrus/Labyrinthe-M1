8dc1cc9c5304b1170e09b0c5fb67d32dc0b65b8f
Christian Vasaune <chrisngangom69@gmail.com>
2021-11-11 14:46:55 +0100

    changement des imports

--- src/main/java/com/tp3equipe3/game/LabyrintheManager.java
+++ src/main/java/com/tp3equipe3/game/LabyrintheManager.java
@@ -7,18 +7,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.FileReader;
 
-<<<<<<< HEAD:src/main/java/com/tp3equipe3/game/LabyrintheManager.java
 import com.tp3equipe3.engine.GamePainter;
-import com.tp3equipe3.entite.Heros;
-import com.tp3equipe3.cases.Case;
-import com.tp3equipe3.cases.CaseMur;
-import com.tp3equipe3.cases.CaseSol;
-=======
-import entite.*;
-import ia.IARandrom;
-import cases.*;
-import engine.*;
->>>>>>> main:JeuTemplate/src/game/LabyrintheManager.java
+import com.tp3equipe3.entite.*;
+import com.tp3equipe3.cases.*;
 
 public class LabyrintheManager{
 
